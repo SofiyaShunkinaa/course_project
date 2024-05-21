37929b065f64fd6102c1bd7fbfe8d07973315452
Sofiya <sofik_shu2@mail.ru>
2024-05-21 23:55:19 +0300

    added links to ther tables

--- src/Entity/CustomItemAttribute.php
+++ src/Entity/CustomItemAttribute.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\CustomItemAttributeRepository;
 use Doctrine\ORM\Mapping as ORM;
+use App\Enum\CustomAttributeType;
 
 #[ORM\Entity(repositoryClass: CustomItemAttributeRepository::class)]
 class CustomItemAttribute
@@ -15,6 +16,13 @@ class CustomItemAttribute
 
     #[ORM\Column(length: 255)]
     private ?string $name = null;
+
+    #[ORM\Column(length: 10, enumType: CustomAttributeType::class)]
+    private ?CustomAttributeType $type = null;
+
+    #[ORM\ManyToOne(inversedBy: 'customItemAttributes')]
+    #[ORM\JoinColumn(nullable:false)]
+    private ?ItemCollection $itemCollection = null;
 
     public function getId(): ?int
     {
@@ -32,4 +40,27 @@ class CustomItemAttribute
 
         return $this;
     }
+
+    public function getType(): ?CustomAttributeType
+    {
+        return $this->type;
+    }
+
+    public function setType(CustomAttributeType $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getItemCollection(): ?ItemCollection{
+        return $this->itemColection;
+    }
+
+    public function setItemCollection(?ItemCollection $itemCollection): static{
+        $this->itemCollection = $itemCollection;
+
+        return $this;
+    }
+    
 }
