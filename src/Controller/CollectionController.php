c9b732663ff02913fadc0c0e9f9ffa552cc9899c
Sofiya <sofik_shu2@mail.ru>
2024-05-22 23:59:17 +0300

    added construct method

--- src/Controller/CollectionController.php
+++ src/Controller/CollectionController.php
@@ -4,18 +4,19 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use App\Form\CollectionType;
 use App\Entity\ItemCollection;
-use App\Controller\EntityManagerInterface;
+use Doctrine\ORM\EntityManagerInterface;
 
 class CollectionController extends AbstractController
 {
-    public function __construct(
-        private EntityManagerInterface $entityManager,
-    ){
+    private EntityManagerInterface $entityManager;
 
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
     }
 
     #[Route('/collections', name: 'app_collection')]
@@ -27,22 +28,22 @@ class CollectionController extends AbstractController
     }
 
     #[Route('/collections/create', name: 'app_collection_create', methods: [Request::METHOD_GET, Request::METHOD_POST])]
-    public function сreate(Request $request): Response
+    public function create(Request $request): Response
     {
         $collection = new ItemCollection();
         $form = $this->createForm(CollectionType::class, $collection);
         $form->handleRequest($request);
 
-        if($form->isSubmitted() && $form->isValid()){
+        if ($form->isSubmitted() && $form->isValid()) {
             $this->entityManager->persist($collection);
             $this->entityManager->flush();
 
-            $this->addFlush('success', "Collection successfully created!");
+            $this->addFlash('success', "Collection successfully created!");
         }
 
         return $this->render('collection/form.html.twig', [
             'action' => 'create',
-            'form' => $form,
+            'form' => $form->createView(),
         ]);
     }
 }
