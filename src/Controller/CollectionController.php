1f0675621fd63502e77325bd9e5f12dee561a12f
Sofiya <sofik_shu2@mail.ru>
2024-05-22 23:52:44 +0300

    added saving logic

--- src/Controller/CollectionController.php
+++ src/Controller/CollectionController.php
@@ -8,9 +8,16 @@ use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Request;
 use App\Form\CollectionType;
 use App\Entity\ItemCollection;
+use App\Controller\EntityManagerInterface;
 
 class CollectionController extends AbstractController
 {
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+    ){
+
+    }
+
     #[Route('/collections', name: 'app_collection')]
     public function index(): Response
     {
@@ -22,11 +29,15 @@ class CollectionController extends AbstractController
     #[Route('/collections/create', name: 'app_collection_create', methods: [Request::METHOD_GET, Request::METHOD_POST])]
     public function сreate(Request $request): Response
     {
-        $form = $this->createForm(CollectionType::class, new ItemCollection());
+        $collection = new ItemCollection();
+        $form = $this->createForm(CollectionType::class, $collection);
         $form->handleRequest($request);
 
         if($form->isSubmitted() && $form->isValid()){
+            $this->entityManager->persist($collection);
+            $this->entityManager->flush();
 
+            $this->addFlush('success', "Collection successfully created!");
         }
 
         return $this->render('collection/form.html.twig', [
