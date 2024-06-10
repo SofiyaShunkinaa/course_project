f0bb47d4458a79d15daf6522a39bb9c04f2ee9e5
Sofiya <sofik_shu2@mail.ru>
2024-06-10 12:17:40 +0300

    added locale subscriber

---
+++ src/EventSubscriber/LocaleSubscriber.php
@@ -0,0 +1,40 @@
+<?php
+// src/EventSubscriber/LocaleSubscriber.php
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class LocaleSubscriber implements EventSubscriberInterface
+{
+    private $defaultLocale;
+
+    public function __construct($defaultLocale = 'en')
+    {
+        $this->defaultLocale = $defaultLocale;
+    }
+
+    public function onKernelRequest(RequestEvent $event)
+    {
+        $request = $event->getRequest();
+        if (!$request->hasPreviousSession()) {
+            return;
+        }
+
+        // Проверяем наличие параметра _locale в запросе
+        if ($locale = $request->attributes->get('_locale')) {
+            $request->getSession()->set('_locale', $locale);
+        } else {
+            // Устанавливаем локаль из сессии, если параметр _locale отсутствует
+            $request->setLocale($request->getSession()->get('_locale', $this->defaultLocale));
+        }
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::REQUEST => [['onKernelRequest', 15]],
+        ];
+    }
+}
